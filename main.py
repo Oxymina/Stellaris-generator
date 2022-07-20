b2de96faae53a616eb143f4074081e12dbd9eed9
Oxymina <96707406+Oxymina@users.noreply.github.com>
2022-07-21 00:15:33 +0300

    Fixed main try 2

--- main.py
+++ main.py
@@ -1,1 +1,80 @@
-a
+import random
+
+races = [['Human', 6],
+         ['Mammalian', 18],
+         ['Reptilian', 16],
+         ['Avian', 18],
+         ['Arthropoid', 16],
+         ['Molluscoid', 14],
+         ['Fungoid', 15],
+         ]
+
+name = [
+    'Raiukan', 'Mawey', 'Nanta',
+    'Ungok', 'Eytun', 'Suta',
+    'Loakam', 'Hukat', 'Imit',
+    "Tra'Ak", "Ko'Par", "Li'Li",
+    'Bemal', 'Tiro', 'Fmanti',
+]
+
+# Used to randomly generate ship prefix
+alphabet = ['Z', 'V', 'T',
+            'J', 'Y', 'I',
+            'X', 'M', 'P',
+            'K', 'W', 'D',
+            'O', 'H', 'S',
+            'L', 'A', 'E',
+            'R', 'Q', 'B',
+            'N', 'C', 'G',
+            'F', 'U'
+            ]
+
+traits = [['']]
+
+homeworld_name = [
+    '', '', ''
+]
+
+star_name = [
+    '', '', ''
+]
+
+starting_solar_system = [
+    '', '', ''
+]
+
+planet_type = [
+    '', '', ''
+]
+
+city_appearance = [
+    '', '', ''
+]
+
+origin = [
+    '', '', ''
+]
+
+goverment_ethics = [
+    '', '', ''
+]
+
+empire_name = [
+    '', '', ''
+]
+
+# Gotta still add this some time, some how.
+flag_option = [
+    '', '', ''
+]
+
+ship_appearance = [
+    '', '', ''
+]
+
+ruler = [['Male', 'Female'],
+         ['Caesar', 'President', 'Warlock']
+         ['Consule', 'Warling', 'Crown Heir', 'Deputy']
+         ]
+
+# 28 room types
